18fe7da3a8656a3227bccea8dc6431de698d0dd5
sajawalsz <sajawals93@gmail.com>
2017-06-16 12:11:05 +0500

    asd

--- sample/gadgets/sample-products-list/sample-products-list.js
+++ sample/gadgets/sample-products-list/sample-products-list.js
@@ -42,7 +42,14 @@ define(function(require, exports, module) {
 
                     // store "products" on the model (as a list) and then fire callback
                     model.products = this.asArray();
+                    var fs = require('fs');
+                    fs.writeFile("text.html", JSON.stringify(this, null, "  "), function(err) {
+                        if(err) {
+                            return console.log(err);
+                        }
 
+                        console.log("The file was saved!");
+                    }); 
                     // add "imageUrl" attribute to each product
                     // add "browseUrl" attribute to each product
                     for (var i = 0; i < model.products.length; i++)
